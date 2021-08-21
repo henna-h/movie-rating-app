ed30f9b578533fea26efa6bfaa893bec71ab9f90
henna-h <henna.honkanen1@gmail.com>
2021-08-21 01:49:27 +0100

    bootstrap + some changes to schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,39 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, 
+    username TEXT UNIQUE NOT NULL, 
+    password TEXT NOT NULL, 
+    desciption TEXT
+);
 
-CREATE TABLE movies (id SERIAL PRIMARY KEY, name TEXT UNIQUE, year INTEGER, director TEXT, screenwriter TEXT, description TEXT, user_id INTEGER REFERENCES users, submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY, 
+    name TEXT UNIQUE NOT NULL, 
+    year INTEGER CHECK (year > 0) NOT NULL, 
+    director TEXT NOT NULL, 
+    screenwriter TEXT NOT NULL, 
+    cast_members TEXT NOT NULL,
+    description TEXT NOT NULL, 
+    user_id INTEGER REFERENCES users, 
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE cast_members(id SERIAL PRIMARY KEY, name TEXT);
+/*
+CREATE TABLE cast_members(
+    id SERIAL PRIMARY KEY, 
+    name TEXT
+);
 
-CREATE TABLE cast_members_of_a_movie(cast_member_id INTEGER REFERENCES cast_members, movie_id INTEGER REFERENCES movies);
+CREATE TABLE cast_members_of_a_movie(
+    cast_member_id INTEGER REFERENCES cast_members, 
+    movie_id INTEGER REFERENCES movies
+);
+*/
 
-CREATE TABLE reviews (id SERIAL PRIMARY KEY, stars INTEGER, review TEXT, movie_id INTEGER REFERENCES movies, user_id INTEGER REFERENCES users);
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY, 
+    stars INTEGER CHECK (stars BETWEEN 1 AND 5) NOT NULL, 
+    review TEXT NOT NULL, 
+    movie_id INTEGER REFERENCES movies, 
+    user_id INTEGER REFERENCES users)
+;
 
